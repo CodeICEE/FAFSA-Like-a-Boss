0c53693314ea55dc742095965f51acca636450db
unknown <vl.joseph09@gmail.com>
2021-06-15 12:43:12 -0400

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+    username: "vladmin",
+    email: "gitvladonrails@gmail.com",
+    password: "Password",
+    is_admin: true
+
+)
+
+Channel.create(
+    channel_name: "Ask an Expert"
+)
+
+Channel.create(
+    channel_name: "General Discussion"
+)
